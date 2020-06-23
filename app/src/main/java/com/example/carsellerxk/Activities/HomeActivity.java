e3b602d372a0138642cff3486c84ec2e635cf0c1
muhamedzeqirii <muhamed.zeqiri@studentet.uni-pr.edu>
2020-06-23 08:17:59 +0200

    Constructing HomeActivity when posts will be displayed. Designing post item

--- app/src/main/java/com/example/carsellerxk/Activities/HomeActivity.java
+++ app/src/main/java/com/example/carsellerxk/Activities/HomeActivity.java
@@ -7,52 +7,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.example.carsellerxk.Activities.MoreActivity;
-import com.example.carsellerxk.Activities.NewPostActivity;
-import com.example.carsellerxk.Activities.ProfileActivity;
-import com.example.carsellerxk.Activities.SavedActivity;
 import com.example.carsellerxk.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity {
 
-
+    BottomNavigationView bottomNavigationView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        BottomNavigationView bottomNavigationView =   findViewById(R.id.bottom_navigation);
-
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.home);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.home:
                         return true;
                     case R.id.newpost:
                         startActivity(new Intent(getApplicationContext()
                                 , Login.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.saved:
                         startActivity(new Intent(getApplicationContext()
                                 , SavedActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.profile:
                         startActivity(new Intent(getApplicationContext()
                                 , ProfileActivity.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.more:
                         startActivity(new Intent(getApplicationContext()
                                 , Login.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;

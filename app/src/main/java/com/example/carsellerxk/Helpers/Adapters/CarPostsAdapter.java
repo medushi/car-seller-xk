21ed4b9a96d4e6d89f49f03577325a896c7b6a4b
muhamedzeqirii <muhamed.zeqiri@studentet.uni-pr.edu>
2020-06-25 07:35:56 +0200

    testing hardcoded dummy data to display in recyclerview.Playing around with images holder,scaleTypes

--- app/src/main/java/com/example/carsellerxk/Helpers/Adapters/CarPostsAdapter.java
+++ app/src/main/java/com/example/carsellerxk/Helpers/Adapters/CarPostsAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.carsellerxk.Interfaces.IItemClickListener;
 import com.example.carsellerxk.Models.NewPostUploadModel;
 import com.example.carsellerxk.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -38,6 +40,17 @@ public class CarPostsAdapter extends RecyclerView.Adapter<CarPostsAdapter.CarPos
     public void onBindViewHolder(@NonNull CarPostsViewHolder holder, int position) {
         //holder.txtPostTitle.setText(carPostModelList.get(position).getCity());
         holder.txtPostTitle.setText(carPostModelList.get(position).getTitle());
+        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostOwner);
+        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage1);
+        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage2);
+        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage3);
+        holder.txtPostOwner.setText("Filani");
+        holder.txtPostTitle.setText("Golf 6 nga Gjermania gjendje te mire");
+        holder.txtPostlocation.setText("Prishtine");
+        holder.txtCarManufacturer.setText("Volkswagen");
+        holder.txtCarAcceleratingType.setText("Automatik");
+        holder.txtCarYearofProduction.setText("2012");
+        holder.txtCarPrice.setText("12500");
     }
 
     @Override
@@ -50,11 +63,22 @@ public class CarPostsAdapter extends RecyclerView.Adapter<CarPostsAdapter.CarPos
     }
 
     public class CarPostsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        TextView txtPostTitle;
-
+        TextView txtPostTitle,txtPostOwner,txtPostlocation,txtCarYearofProduction,txtCarAcceleratingType,txtCarPrice,txtCarManufacturer;
+        ImageView ivPostOwner,ivPostImage1,ivPostImage2,ivPostImage3;
         public CarPostsViewHolder(View view) {
             super(view);
             txtPostTitle=view.findViewById(R.id.txtPostTitle);
+            ivPostOwner=view.findViewById(R.id.ivPostOwner);
+            txtPostOwner=view.findViewById(R.id.txtPostOwnerName);
+            txtPostlocation=view.findViewById(R.id.txtPostLocation);
+            txtCarYearofProduction=view.findViewById(R.id.txtCarYearOfProduction);
+            txtCarAcceleratingType=view.findViewById(R.id.txtCarAcceleratingType);
+            txtCarManufacturer=view.findViewById(R.id.txtCarManufacturer);
+            txtCarPrice=view.findViewById(R.id.txtCarPrice);
+            ivPostImage1=view.findViewById(R.id.ivpostImage1);
+            ivPostImage2=view.findViewById(R.id.ivpostImage2);
+            ivPostImage3=view.findViewById(R.id.ivpostImage3);
+
         }
         @Override
         public void onClick(View view){

f49e132d5c07b7566aca463476471ff9978c0311
muhamedzeqirii <muhamed.zeqiri@studentet.uni-pr.edu>
2020-06-30 02:17:56 +0200

    Replacing hardcoded dummy data with data fetched from firebase rtd, Handling default image and default profile picture, Rescaling images when set in image views. Shortening recyclerview to unhide from nav bar

--- app/src/main/java/com/example/carsellerxk/Helpers/Adapters/CarPostsAdapter.java
+++ app/src/main/java/com/example/carsellerxk/Helpers/Adapters/CarPostsAdapter.java
@@ -4,15 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.carsellerxk.Activities.HomeActivity;
+import com.example.carsellerxk.Helpers.Statics;
 import com.example.carsellerxk.Interfaces.IItemClickListener;
 import com.example.carsellerxk.Models.NewPostUploadModel;
+import com.example.carsellerxk.Models.PostsModel;
 import com.example.carsellerxk.R;
 import com.squareup.picasso.Picasso;
 
@@ -20,11 +25,11 @@ import java.util.List;
 
 public class CarPostsAdapter extends RecyclerView.Adapter<CarPostsAdapter.CarPostsViewHolder> {
 
-    private List<NewPostUploadModel> carPostModelList;
+    private List<PostsModel> carPostModelList;
     private Context context;
     private IItemClickListener itemClickListener;
 
-    public CarPostsAdapter(List<NewPostUploadModel> carPostsList,Context context) {
+    public CarPostsAdapter(List<PostsModel> carPostsList,Context context) {
         this.carPostModelList = carPostsList;
         this.context=context;
     }
@@ -37,20 +42,31 @@ public class CarPostsAdapter extends RecyclerView.Adapter<CarPostsAdapter.CarPos
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CarPostsViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull  CarPostsViewHolder holder, int position) {
         //holder.txtPostTitle.setText(carPostModelList.get(position).getCity());
-        holder.txtPostTitle.setText(carPostModelList.get(position).getTitle());
-        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostOwner);
-        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage1);
-        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage2);
-        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage3);
-        holder.txtPostOwner.setText("Filani");
-        holder.txtPostTitle.setText("Golf 6 nga Gjermania gjendje te mire");
-        holder.txtPostlocation.setText("Prishtine");
-        holder.txtCarManufacturer.setText("Volkswagen");
-        holder.txtCarAcceleratingType.setText("Automatik");
-        holder.txtCarYearofProduction.setText("2012");
-        holder.txtCarPrice.setText("12500");
+//        holder.txtPostTitle.setText(carPostModelList.get(position).getTitle());
+//        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostOwner);
+//        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage1);
+//        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage2);
+//        Picasso.get().load("https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").noFade().into(holder.ivPostImage3);
+//        holder.txtPostOwner.setText("Filani");
+//        holder.txtPostTitle.setText("Golf 6 nga Gjermania gjendje te mire");
+//        holder.txtPostlocation.setText("Prishtine");
+//        holder.txtCarManufacturer.setText("Volkswagen");
+//        holder.txtCarAcceleratingType.setText("Automatik");
+//        holder.txtCarYearofProduction.setText("2012");
+//        holder.txtCarPrice.setText("12500");
+        holder.txtPostOwner.setText(carPostModelList.get(position).getPostOwnerName());
+        holder.txtPostlocation.setText(carPostModelList.get(position).getPostLocation());
+        holder.txtCarYearofProduction.setText(carPostModelList.get(position).getYearOfProduction()+"");
+        holder.txtCarManufacturer.setText(carPostModelList.get(position).getManufacturer());
+        holder.txtCarAcceleratingType.setText(carPostModelList.get(position).getAccType());
+        holder.txtPostTitle.setText(carPostModelList.get(position).getPostTitle());
+        Picasso.get().load(Statics.DefaultPostOwnerImage).noFade().into(holder.ivPostOwner);
+        Picasso.get().load(carPostModelList.get(position).getImg1()).noFade().into(holder.ivPostImage1);
+        Picasso.get().load(carPostModelList.get(position).getImg2()).noFade().into(holder.ivPostImage2);
+        Picasso.get().load(carPostModelList.get(position).getImg3()).noFade().into(holder.ivPostImage3);
+
     }
 
     @Override
@@ -60,11 +76,13 @@ public class CarPostsAdapter extends RecyclerView.Adapter<CarPostsAdapter.CarPos
 
     public void setClickListener(IItemClickListener iItemClickListener){
         this.itemClickListener=iItemClickListener;
+
     }
 
     public class CarPostsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView txtPostTitle,txtPostOwner,txtPostlocation,txtCarYearofProduction,txtCarAcceleratingType,txtCarPrice,txtCarManufacturer;
         ImageView ivPostOwner,ivPostImage1,ivPostImage2,ivPostImage3;
+        Button btnBuy,btnAddToFavorites;
         public CarPostsViewHolder(View view) {
             super(view);
             txtPostTitle=view.findViewById(R.id.txtPostTitle);
@@ -78,13 +96,19 @@ public class CarPostsAdapter extends RecyclerView.Adapter<CarPostsAdapter.CarPos
             ivPostImage1=view.findViewById(R.id.ivpostImage1);
             ivPostImage2=view.findViewById(R.id.ivpostImage2);
             ivPostImage3=view.findViewById(R.id.ivpostImage3);
+            btnBuy=view.findViewById(R.id.btnBuy);
+            btnAddToFavorites=view.findViewById(R.id.btnAddToFavorites);
+            btnBuy.setOnClickListener(this);
+            btnAddToFavorites.setOnClickListener(this);
 
         }
         @Override
-        public void onClick(View view){
-            if(itemClickListener!=null){
-                itemClickListener.onClick(view,getAdapterPosition());
+        public void onClick(View view) {
+            if(view.getId()==R.id.btnBuy){
+                Toast.makeText(context,"bought succesfully",Toast.LENGTH_LONG).show();
             }
+            else if(view.getId()==R.id.btnAddToFavorites)
+                Toast.makeText(context,"Added to favorites",Toast.LENGTH_LONG).show();
         }
     }
 }
---
+++ app/src/main/java/com/example/carsellerxk/Helpers/FireBaseRTDHelper.java
@@ -0,0 +1,25 @@
+package com.example.carsellerxk.Helpers;
+
+import com.example.carsellerxk.Interfaces.IOnGetDataListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+public class FireBaseRTDHelper {
+    public static void readData(DatabaseReference ref, final IOnGetDataListener listener) {
+        listener.onStart();
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listener.onSuccess(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                listener.onFailure();
+            }
+        });
+
+    }
+}

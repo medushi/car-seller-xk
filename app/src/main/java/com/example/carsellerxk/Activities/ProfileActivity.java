8b90ad0dd02b2dd550b116035be08f76a5221eb2
bledi <bledi-kastratii@live.com>
2020-07-01 01:27:36 +0200

    Ndryshimi i profilit te perdoruesit. Shtimi dhe perdorimi i fragmenteve ne nje activity.

--- app/src/main/java/com/example/carsellerxk/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/carsellerxk/Activities/ProfileActivity.java
@@ -1,40 +1,28 @@
 package com.example.carsellerxk.Activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-import android.content.Intent;
-import android.view.MenuItem;
+import com.example.carsellerxk.FragmentProfile;
+import com.example.carsellerxk.FragmentSettings;
 import com.example.carsellerxk.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
-
-//        Button bLogout;
-//        EditText etUsername;
-//        EditText etEmail;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-//        etUsername = findViewById(R.id.etUsername);
-//        etEmail = findViewById(R.id.etEmail);
-//        bLogout = findViewById(R.id.bLogout);
-//
-//        bLogout.setOnClickListener(this);
-//    }
-//    @Override
-//    public void onClick(View v) {
-//            switch(v.getId()){
-//            case R.id.bLogout:
-//
-//            startActivity(new Intent(this, Login.class));
-//            break;
-//            }
         BottomNavigationView bottomNavigationView =   findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setSelectedItemId(R.id.profile);
@@ -70,4 +58,24 @@ public class ProfileActivity extends AppCompatActivity /*implements View.OnClick
             }
         });
     }
+
+    public void ChangeFragment(View view) {
+        Fragment fragment;
+
+        if(view == findViewById(R.id.button)) {
+            fragment = new FragmentProfile();
+            androidx.fragment.app.FragmentManager fm = getSupportFragmentManager();
+            androidx.fragment.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if(view == findViewById(R.id.button2)) {
+            fragment = new FragmentSettings();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+    }
+
 }

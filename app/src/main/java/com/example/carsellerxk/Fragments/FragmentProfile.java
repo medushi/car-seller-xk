c01a1ba3a98bfa1bdb86d348cba03876d3273144
muhamedzeqirii <muhamed.zeqiri@studentet.uni-pr.edu>
2020-07-02 17:25:27 +0200

    Move fragments to their folder

--- app/src/main/java/com/example/carsellerxk/Fragments/FragmentProfile.java
+++ app/src/main/java/com/example/carsellerxk/Fragments/FragmentProfile.java
@@ -1,4 +1,4 @@
-package com.example.carsellerxk;
+package com.example.carsellerxk.Fragments;
 
 
 import android.os.Bundle;
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.example.carsellerxk.Helpers.LoginHelper;
 import com.example.carsellerxk.Models.MyProfileModel;
+import com.example.carsellerxk.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
